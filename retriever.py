efb2556571d38f3b267e8f05ad96aa41dab40297
Payal Parida <payalparida@Payals-MacBook-Air.local>
2025-08-14 17:09:11 +0530

    Code to retrieve the embeddings, create prompts for the LLM and create an UI to response to the prompts.

---
+++ retriever.py
@@ -0,0 +1,50 @@
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import Ollama
+import streamlit as st
+
+def getDB():
+    embeddings = (
+        OllamaEmbeddings(model="gemma2:2b")
+    )
+    db = FAISS.load_local("VectorDB/index",
+                          embeddings,
+                          allow_dangerous_deserialization=True)
+    
+    return db
+
+# Prompt:
+
+prompt = ChatPromptTemplate.from_template(
+    """
+    You are a helpful assistant. If the answer cannot be found in the context, say "The exact answer is not defined, However, the relevant context is as follows:" and answer the the following question based on the below context.
+    <context>
+    {context}
+    </context>
+    
+    Question: {input}
+    Rules:
+    - If the answer is in the context, copy it exactly without adding extra details.
+    """
+    )
+
+llm = Ollama(model="gemma2:2b")
+doc_chain = create_stuff_documents_chain(llm,prompt)
+
+db = getDB()
+
+retriever = db.as_retriever()
+ret_chin = create_retrieval_chain(retriever,doc_chain)
+
+# streamlit code
+
+st.title("GreenAsha Chatbot Testing")
+input_text = st.text_input("Ask a question")
+
+if input_text:
+    res = ret_chin.invoke({"input": input_text})
+    st.write(res["answer"])
+
